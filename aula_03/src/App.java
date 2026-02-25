5fe29b8367f08dc55a2704e0e4c3fcdb67114258
Marcos <marcosjoaomj76@gmail.com>
2026-02-25 19:59:38 -0300

    comentarios adicionado

--- aula_03/src/App.java
+++ aula_03/src/App.java
@@ -1,5 +1,12 @@
+/*
+    Este código é um exemplo de classe Java
+    UC:Linguagem de programação
+*/
+// Declara a class App
 public class App {
+    // Metodo principal
     public static void main(String[] args) throws Exception {
+        // Escreve um texto no terminal
         System.out.println("Hello, World!");
     }
 }

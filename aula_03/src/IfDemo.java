dd605285bf0ed72ae2296458c4789869536b2910
Marcos <marcosjoaomj76@gmail.com>
2026-02-25 21:59:54 -0300

    Atividade de IfDemo

---
+++ aula_03/src/IfDemo.java
@@ -0,0 +1,14 @@
+public class IfDemo {
+    public static void main(String[] args) {
+        // Declara 3 variáveis do tipo inteiro
+        int a = 5, b = 3, c = 7;
+        // Exibi a linha somente se a condição for verdadeira
+        if (a > b) System.out.println("a é maior que b");
+        if (a > c) System.out.println( "a é maior que c");
+        if (b > a) System.out.println("b é maior que a");
+        if (b > c) System.out.println( "b maior que c");
+        if (c > a) System.out.println("c é maior que a");
+        if (c > b) System.out.println("c é maior que b");
+            
+    }
+}

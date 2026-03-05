6ee2b9f81e04ea21aa4b58a7704cdb16ab11b533
Marcos <marcosjoaomj76@gmail.com>
2026-03-04 21:00:26 -0300

    Ler Dados Variaveis

---
+++ aula-04/src/ExemploLerDadosVariados.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ExemploLerDadosVariados {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Digite seu primeiro nome: ");
+        String primeiroNome = scan.next();
+        System.out.print("Digite sua idade: ");
+        int idade = scan.nextInt();
+        System.out.print("Digite sua altura: ");
+        double altura = scan.nextDouble();
+        System.out.println("Olá, " + primeiroNome + 
+          " você tem " + idade + " anos e tem " + altura + " metros de altura" 
+        );
+
+        scan.close();
+    }   
+}

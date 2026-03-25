7f0b7d426e9c7c2a37a92070e455416396de8729
MarcosVxn <marcosjoaomj76@gmail.com>
2026-03-25 19:56:43 -0300

    ForAninhado corrigido

--- aula_06/src/ForAninhado.java
+++ aula_06/src/ForAninhado.java
@@ -2,12 +2,20 @@ import java.util.Scanner;
 
 public class ForAninhado {
     public static void main(String[] args) {
+        // objeto para leitura de dados do teclado
         Scanner scan = new Scanner(System.in);
+        // solicitado ao usuário um numero
         System.out.print("Digite um numero: ");
-        int numuser = scan.nextInt();
-        for (int linha = 0; linha < numuser; linha++){
-            for (int coluna = 0; coluna < numuser; coluna++){
-                System.out.print("*");
+        // le o valor digitado pelo usuário
+        int lado = scan.nextInt();
+        for (int linha = 0; linha < lado; linha++){
+            for (int coluna = 0; coluna < lado; coluna++){
+                if (linha == 0 || linha == lado - 1 || coluna == 0 || coluna == lado -1) {
+                    System.out.print("*");                    
+                } else {
+                    System.out.print(" ");
+                }
+
             }
             System.out.println();
         }

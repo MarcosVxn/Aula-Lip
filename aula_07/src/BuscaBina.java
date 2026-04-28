7efeb694ce33bd1e10ce9a1e481c2dec6ded8f08
Marcos <marcosjoaomj76@gmail.com>
2026-04-28 11:23:20 -0300

    Update BuscaBina.java

--- aula_07/src/BuscaBina.java
+++ aula_07/src/BuscaBina.java
@@ -1,6 +1,9 @@
 public class BuscaBina {
     public static void main(String[] args) {
+        //Variaveis
        int[] num = new int[101];
+
+       // Busca 
        for(int i = 0; i < num.length; i++){
         num[i] = (int)(Math.random()* 100)+ 1;
 
